c20c196c69c4c521a3b8c0c581a178f5f9155566
ahmettb <boztemurahmet7@gmail.com>
2024-04-03 15:44:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+
+
+public class Main {
+
+
+    //static void main
+    /*
+        void main 2
+     */
+
+    /**
+     *
+     * @param args
+     * main static
+     */
+    public static void main(String[] args) {
+
+        //konsol yazdirma
+        System.out.println("Hello world!");//konsol2 yazdırma
+    }
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * Test.java classi
+ */
+
+//public test
+public class Test {
+
+
+    //FDSFDSFSDFDS
+    public void getir(){
+
+        //SDFDSFDSFSDF
+
+    };
+
+
+    /**
+     *
+     * @param
+     * @return
+     */
+    /*
+    DSADASDAS
+    ADSA
+    ASDSD
+     */
+    public String getir2(int a) {
+
+        /*
+        ASDSADAS
+        ASDAD
+         */
+        a = 12;
+        return "deneme"; //FDSFSFDS
+        //FDSFDSFDS
+    };
+    /*
+    DASDSA
+    ASDSA
+     */
+
+    /**
+     *
+     * ASDSADSA
+     * @return
+     */
+    public List<String> a()
+    {
+        //DSFSFSD
+        /**
+         * SDADSADSAD
+         */
+        return new ArrayList<>();
+    }
+
+}
